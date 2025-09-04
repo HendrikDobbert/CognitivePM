de751111ecc8fa8600df89917cf2354ff9a05771
Hendrik Dobbert <HendrikDobbert@gmail.com>
2025-09-04 09:47:06 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PLACEHOLDER_API_KEY",
-  authDomain: "PLACEHOLDER_AUTH_DOMAIN",
-  projectId: "PLACEHOLDER_PROJECT_ID",
-  storageBucket: "PLACEHOLDER_STORAGE_BUCKET",
-  messagingSenderId: "PLACEHOLDER_MESSAGING_SENDER_ID",
-  appId: "PLACEHOLDER_APP_ID"
+  "projectId": "cognitivepm",
+  "appId": "1:791515009157:web:db5becc106885aa16d59ec",
+  "storageBucket": "cognitivepm.firebasestorage.app",
+  "apiKey": "AIzaSyB1bN7mMHbHomstyS2f6pfttGVZZVhXSdU",
+  "authDomain": "cognitivepm.firebaseapp.com",
+  "messagingSenderId": "791515009157"
 };
 
 // Initialize Firebase
