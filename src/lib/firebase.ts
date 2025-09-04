5cada63e184cf708842c791d0d7e0b1d2b157f71
Hendrik Dobbert <HendrikDobbert@gmail.com>
2025-09-04 10:56:24 +0000

    Hier: // Import the functions you need from the SDKs you need import { i

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,16 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "cognitivepm",
-  "appId": "1:791515009157:web:db5becc106885aa16d59ec",
-  "storageBucket": "cognitivepm.firebasestorage.app",
-  "apiKey": "AIzaSyB1bN7mMHbHomstyS2f6pfttGVZZVhXSdU",
-  "authDomain": "cognitivepm.firebaseapp.com",
-  "messagingSenderId": "791515009157"
+  apiKey: "AIzaSyB1bN7mMHbHomstyS2f6pfttGVZZVhXSdU",
+  authDomain: "cognitivepm.firebaseapp.com",
+  projectId: "cognitivepm",
+  storageBucket: "cognitivepm.firebasestorage.app",
+  messagingSenderId: "791515009157",
+  appId: "1:791515009157:web:db5becc106885aa16d59ec"
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
